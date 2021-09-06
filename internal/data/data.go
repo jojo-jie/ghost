345279e7676ad12acc8b7cec6b121f6e89775c22
jojo-jie <18624275868@163.com>
2021-09-06 21:55:45 +0800

    gorm init

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"fmt"
 	"ghost/internal/conf"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"time"
 )
 
 // ProviderSet is data providers.
@@ -12,12 +16,31 @@ var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
 // Data .
 type Data struct {
 	// TODO wrapped database client
+	Db *gorm.DB
 }
 
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+
+	dsn := fmt.Sprintf("%s?charset=utf8mb4&parseTime=True&loc=Local", c.Database.Source)
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN: dsn,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, nil, err
+	}
+	sqlDb, err := db.DB()
+	if err != nil {
+		return nil, nil, err
+	}
+	sqlDb.SetMaxIdleConns(int(c.Database.GetSetMaxIdleConns()))
+	sqlDb.SetMaxOpenConns(int(c.Database.GetSetMaxOpenConns()))
+	sqlDb.SetConnMaxLifetime(time.Duration(int(c.Database.GetSetConnMaxLifetime().GetSeconds())))
+
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{
+		Db: db,
+	}, cleanup, nil
 }
