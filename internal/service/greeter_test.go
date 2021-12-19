5c54aad54ba405f26554ee4a9ae908e6e75d0b47
jojo-jie <18624275868@163.com>
2021-12-19 21:57:58 +0800

    multiplex

--- internal/service/greeter_test.go
+++ internal/service/greeter_test.go
@@ -17,7 +17,10 @@ import (
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"io"
 	nhttp "net/http"
+	"os/signal"
 	"strconv"
+	"sync"
+	"syscall"
 	"unsafe"
 
 	jwtv4 "github.com/golang-jwt/jwt/v4"
@@ -155,4 +158,90 @@ func TestUintptr(t *testing.T) {
 	b := unsafe.Pointer(uintptr(unsafe.Pointer(w)) + unsafe.Offsetof(w.b))
 	*((*int)(b)) = 10
 	t.Log(w.a, w.b)
+}
+
+
+func TestFanIn(t *testing.T) {
+	// create two sample message and stop channels
+	mc1, sc1 := generate("message from generator 1", 200*time.Millisecond)
+	mc2, sc2 := generate("message from generator 2", 200*time.Millisecond)
+
+	// multiplex message channels
+	mmc, wg1 := multiplex(mc1, mc2)
+
+	// create errs channel for graceful shutdown
+	errs := make(chan error)
+
+	// wait for interrupt or terminate signal
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s signal received", <-sc)
+	}()
+
+	// wait for multiplexed messages
+	wg2 := &sync.WaitGroup{}
+	wg2.Add(1)
+	go func() {
+		defer wg2.Done()
+		for m := range mmc {
+			t.Log(m)
+		}
+	}()
+
+	// wait for errors
+	if err := <-errs; err != nil {
+		t.Log(err.Error())
+	}
+
+	stopGenerating(mc1, sc1)
+	stopGenerating(mc2, sc2)
+	wg1.Wait()
+
+	// close multiplexed messages channel
+	close(mmc)
+	wg2.Wait()
+}
+
+func generate(message string, interval time.Duration) (chan string, chan struct{}) {
+	mc := make(chan string)
+	sc := make(chan struct{})
+
+	go func() {
+		defer func() {
+			close(sc)
+		}()
+		for {
+			select {
+			case <-sc:
+				return
+			default:
+				time.Sleep(interval)
+				mc <- message
+			}
+		}
+	}()
+	return mc, sc
+}
+
+func stopGenerating(mc chan string, sc chan struct{}) {
+	sc <- struct{}{}
+	close(mc)
+}
+
+// 多路复用函数
+func multiplex(mcs ...chan string) (chan string, *sync.WaitGroup) {
+	mmc := make(chan string)
+	wg := &sync.WaitGroup{}
+
+	for _, mc := range mcs {
+		wg.Add(1)
+		go func(mc chan string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			for m := range mc {
+				mmc <- m
+			}
+		}(mc, wg)
+	}
+	return mmc, wg
 }
